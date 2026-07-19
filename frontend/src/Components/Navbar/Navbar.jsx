702523e2d46b8f8038606d13aad44ec5089f6cc6
Homam Alzoubi <homamalzoubi123>
2026-07-19 20:40:40 +0200

    [Ho-1111] Add Navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+  <div className="navbar">
+            <div className="navbar__Container">
+                <span className="navbar__logo">RecipeShare</span>
+                <div className="navbar_pages">
+                    <span>Home</span>
+                    <span>Recipes</span>
+                    <span>Contact</span>
+                    <span>About</span>
+                  </div>
+                    <div className="navbar__navItems">
+                        <button className="navbar__navButton">Register</button>
+                        <button className="navbar__navButton">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
