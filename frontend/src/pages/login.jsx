a1256e163141ce818ccf47025042d23e2e54d42d
Homam Alzoubi <homamalzoubi123>
2026-08-02 17:38:32 +0200

    [Ho-1117] use "Input compent " in login and register

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,11 @@
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import InputField from "../Components/InputField/InputField";
 
-const Login = () => {
+const Login = (
+    
+) => {
     const [formData, setFormData] = useState({
         email: "",
         password: ""
@@ -28,7 +31,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            // 2. إرسال طلب POST إلى الـ API الخاص بك
+       
             const response = await fetch("http://localhost:5082/api/auth/login", {
                 method: "POST",
                 headers: {
@@ -44,11 +47,10 @@ const Login = () => {
             const data = await response.json();
             localStorage.setItem("token", data.token);
             if (!response.ok) {
-                // إظهار نص الخطأ القادم من الـ Backend
+                
                 throw new Error(data.message || data || "Registrierung fehlerhaft");
             }
-
-            // 3. النجاح وتوجيه المستخدم لصفحة تسجيل الدخول
+         
             setSuccess("Erfolgreich angemeldet! Sie werden weitergeleitet...");
             setTimeout(() => {
                 navigate("/");
@@ -66,44 +68,63 @@ const Login = () => {
         <div className="login">
             <div className="login__card">
                 <Link to="/">
-                <button className="login__card__back">Zurück</button>
-            </Link>
-                <h2 className="login__title">Willkommen</h2>
-                {error && <div style={{ color: "#e74c3c", marginBottom: "1rem" }}>{error}</div>}
-                {success && <div style={{ color: "#2ecc71", marginBottom: "1rem" }}>{success}</div>}
-
-                <div className="login__card__field">
-                    <label className="login__card__label">E-Mail-Adresse</label>
-                    <input
-                        className="login__card__input"
-                        type="email"
-                        placeholder="E-Mail-Adresse"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="login__card__field">
-                    <div className="login__card__label__row">
-                        <label className="login__card__label">Passwort</label>
-                        <a className="login__link__passwort" href="#">Passwort vergessen?</a>
+                     <button className="login__card__back">Zurück</button>
+                </Link>
+                <h2
+                    className="login__title">
+                    Willkommen
+                </h2>
+                {error &&
+                    <div
+                        style={{
+                            color: "#e74c3c",
+                            marginBottom: "1rem"
+                        }}>
+                        {error}
                     </div>
-                    <input
-                        className="login__card__input"
-                        type="password"
-                        placeholder="Passwort"
-                        name="password"
-                        value={formData.password}
-                        required
-                        onChange={handleChange}
-                    />
+                }
+                {success &&
+                    <div
+                        style={{
+                            color: "#2ecc71",
+                            marginBottom: "1rem"
+                        }}>
+                        {success}
+                    </div>}
+
+                <InputField
+                    label="E-Mail-Adresse"
+                    type="email"
+                    name="email"
+                    placeholder="E-Mail-Adresse"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <InputField
+                    label="Passwort"
+                    type="password"
+                    name="password"
+                    placeholder="Passwort"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+                <div
+                    className="login__card__label__row">
+                    <a
+                        className="login__link__passwort"
+                        href="#">
+                        Passwort vergessen?
+                    </a>
                 </div>
+               
                 <button
-                    type="submit" disabled={loading}
-                    className="login__card__button" onClick={handleSubmit}>
+                    type="submit"
+                    disabled={loading}
+                    className="login__card__button"
+                    onClick={handleSubmit}>
                 {loading ? "Anmelden..." : "Anmelden"}
                 </button>
+
                 <p className="login__card__footer">
                     Noch kein Konto?
                     <a
