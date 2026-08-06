c8d8456a4aa85678a71821a751269f5a47e7f1e0
Homam Alzoubi <homamalzoubi123>
2026-08-06 21:31:59 +0200

    [Ho-1118] add recipe creation feature and persist auth state in localStorage

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,31 +1,39 @@
-
 import Pourder from "../assets/pourder.svg";
 import acaunt from "../assets/account.svg";
 import home from "../assets/home.svg";
 import "./Header.scss"
 import { useState } from "react";
+//import { useContext } from "react";
+//import { AuthContext } from "../context/AuthContext";
 
 function Header() {
+    //const {tocken ,logout} = useContext(AuthContext);
     const [isOpen, setIsOpen] = useState(false);
-
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
+
     return (
-            <header className="header__container">
-                <span className="header__container__logo"  />
+        <header className="header__container">
+            <span className="header__container__logo"  />
             <h2 className="header__container__title">Recipe Share</h2>
-             <div className="header__container__search__container">
+            <div className="header__container__search__container">
                 <span className="header__container__search__icon" />
-                < input className="header__container__search" type="text" placeholder="Search for recipes..." />
-             </div>
+                <input
+                    className="header__container__search"
+                    type="text"
+                    placeholder="Search for recipes..."
+                   
+                />
+               
+            </div>
 
             <a className="header__container__link__home" href="/">
-            <img className="header__container__home" src={home} alt="home" />
+                <img className="header__container__home" src={home} alt="home" />
             </a>
             <div className="header__container__dropdowns">
                 <button className="header__container__button" onClick={toggleDropdown}>
-                   <img className="header__container__account" src={acaunt} alt="account" />                
+                    <img className="header__container__account" src={acaunt} alt="account" />
                 </button>
                 {isOpen && (<div className="header__container__dropdown">
                     <a className="header__container__link" href="/login">Login</a>
@@ -35,8 +43,8 @@ function Header() {
                 </div>)}
             </div>
             <img className="header__container__pourder" src={Pourder} alt="pourder" />
-            </header>
-  );
+        </header>
+    );
 }
 
 export default Header;
