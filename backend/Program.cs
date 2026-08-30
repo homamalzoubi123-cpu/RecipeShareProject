d2fd6c2348f9e6e1ab9c70afba38cb2d14aa1745
Homam Alzoubi <homamalzoubi123>
2026-08-30 21:54:06 +0200

    test2

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.IdentityModel.Tokens;
 using RecipeShare.Api;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 // 1. إضافة قاعدة البيانات
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
