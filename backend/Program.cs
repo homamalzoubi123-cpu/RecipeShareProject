95fd698f583543d329221b517ef3b289924a7a75
Homam Alzoubi <homamalzoubi123>
2026-08-30 20:29:11 +0200

    Ubdate url

--- backend/Program.cs
+++ backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
         policy => policy.AllowAnyOrigin()
             "http://localhost:5173",
             "http://localhost:3000",
-            "https://recipe-share-project.vercel.app"
+            "https://recipe-share-project-vercel.vercel.app"
             ) // منافذ Vite و React
                         .AllowAnyHeader()
                         .AllowAnyMethod());
