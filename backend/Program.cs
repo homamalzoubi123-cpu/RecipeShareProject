f9d2a73384fc642be6051541c7f76f137a321413
Homam Alzoubi <homamalzoubi123>
2026-09-11 21:47:14 +0200

    m

--- backend/Program.cs
+++ backend/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
         policy => policy.AllowAnyOrigin()
+        policy => policy.AllowAnyOrigin()
             "http://localhost:5173",
             "http://localhost:3000",
             "https://recipe-share-project-eight.vercel.app"
